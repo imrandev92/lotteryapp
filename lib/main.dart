5e05d28e5ac6217a69d7709c72bfca8b85220104
imrandev92 <152307234+imrandev92@users.noreply.github.com>
2023-12-08 06:21:48 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -12,30 +12,59 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int x = 0;
+  Random random =   Random();
+  int y = 0 ;
+
   @override
   Widget build(BuildContext context) {
-    print('rebuild');
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App imran khan'),
+          title: Center(child: Text('Lottery App')),
         ),
-        body: SafeArea(
-            child: Center(
-                child: Text(
-          x.toString(),
-          style: TextStyle(fontSize: 50),
-        ))),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(child: Text('Lottery wining number is 4')),
+            SizedBox(height: 20,),
+            Container(
+              height: y == 4 ? 600 : 250,
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: y== 4 ?
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.done , color: Colors.green , size: 35,),
+                    Text('Congratulation you have won the lottery, your number is $y \n ' , textAlign: TextAlign.center,),
+                  ],
+                ):
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(Icons.error , color: Colors.red , size: 35,),
+                    Text('Better luck for next time your number is $y \n try again ' , textAlign: TextAlign.center,),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            x++;
-          setState(() {
-
-          });
-            print(x.toString());
+            y = random.nextInt(6);
+            print(y);
+            setState(() {});
           },
-          child: Icon(Icons.add),
+          child: Icon(Icons.refresh),
         ),
       ),
     );
