4b26a1aeec505af0776394abf7c9688a03ce9be3
MImranDev <im136937@gmail.com>
2023-12-07 23:48:42 -0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -4,30 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  int x = 12;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int x = 0;
   @override
   Widget build(BuildContext context) {
+    print('rebuild');
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App imran khan'),
         ),
-        body:SafeArea(
-            child: Center(child: Text(x.toString(), style: TextStyle(fontSize: 50),))
-            ),
+        body: SafeArea(
+            child: Center(
+                child: Text(
+          x.toString(),
+          style: TextStyle(fontSize: 50),
+        ))),
         floatingActionButton: FloatingActionButton(
-          onPressed:(){
+          onPressed: () {
             x++;
+          setState(() {
 
+          });
             print(x.toString());
           },
           child: Icon(Icons.add),
         ),
-        ),
-      );
+      ),
+    );
   }
 }
-
